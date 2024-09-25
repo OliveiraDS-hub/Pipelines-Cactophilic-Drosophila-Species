7e05b5be3c02698fe2ebfef7349616dc5a315f52
Daniel S Oliveira <daniel.sdo2015@gmail.com>
2024-09-25 15:32:07 +0200

    Create featurecounts.sh

---
+++ Differential-expression/featurecounts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+species_specific_gff=""
+
+for bam_file in *bam; do
+  out_name=$(basename "$bam_file" | sed 's/.bam//g')
+  featureCounts -a $species_specific_gff \
+  "$bam_file" \
+  -o "$out_name".tsv \
+  -p -B  -C -T 20 -t exon -g gene
+  cut -f1,7,8 "$out_name".tsv > tmp.tmp && mv tmp.tmp "$out_name".tsv
+done
